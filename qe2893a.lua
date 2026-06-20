1f4715bdfdd7fbf3f2d70b2d0ab3347681b17471
marsmercuryt231 <earth4988@gmail.com>
2026-06-20 11:15:24 -0400

    Update qe2893a.lua

--- qe2893a.lua
+++ qe2893a.lua
@@ -263,7 +263,7 @@ local mainGui = player.PlayerGui:WaitForChild("MainGui")
 
 mainGui.DescendantAdded:Connect(function(child)
     if child.Name == "BattleGui" then
-        task.wait(0.2)
+        task.wait(0.5)
         local plat = game.Workspace:FindFirstChild("Plat21", true)
         if plat then
             onEncounterStart(plat)
@@ -282,14 +282,15 @@ while true do
         if v:IsA("Model") and string.find(string.lower(v.Name), "trainer", 1, true) then
             v:Destroy()
         end
-        if v.Name == "Door" and v.Parent.Name ~= "Gate" then
+        if (v.Name == "Door" and v.Parent.Name ~= "Gate") or v.Name == "Grass" then
             v:Destroy()
         end
+        
     end
     task.wait(1)
 end
 
-    
+
    
 --771860314 particle id
 
